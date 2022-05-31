3a1f9e60bb46024593c6d9e645f8d363dfc56b43
vvsprakash <v.v.s.prakash143@gmail.com>
2022-05-31 23:05:22 +0530

    new

---
+++ src/org/client/Client.java
@@ -0,0 +1,15 @@
+package org.client;
+
+public class Client {
+	public static void main(String[] args) {
+		String s ="Java";
+		String upperCase = s.toUpperCase();
+		String lowerCase = s.toLowerCase();
+		boolean startsWith = s.startsWith("j");
+		boolean endsWith = s.endsWith("a");
+		boolean equals = s.equals(lowerCase);
+		
+	}
+	
+
+}
